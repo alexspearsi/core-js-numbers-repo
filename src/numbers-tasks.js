25bac230d641c5bd20a9df443c2d2c71af090d64
Alex <strelchenko.aleksandr@yandex.ru>
2024-11-07 11:15:45 +0200

    update: complete task

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -234,8 +234,10 @@ function isPrime(n) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  return !Number.isNaN(Number.parseInt(value, 10))
+    ? Number.parseInt(value, 10)
+    : def;
 }
 
 /**
